b121ae1b93ce3e7c935f9b28c1d92cb38a3e2072
Júlio Hidemi Kamizato <jkamizato@gmail.com>
2019-11-04 22:17:43 -0300

    DMD-000: Add comand phpcbf and phpcs. Hello world form.

---
+++ docroot/modules/custom/hello_world/src/Form/SalutationConfigurationForm.php
@@ -0,0 +1,62 @@
+<?php
+
+
+namespace Drupal\hello_world\Form;
+
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class SalutationConfigurationForm extends ConfigFormBase {
+
+  /**
+   * Gets the configuration names that will be editable.
+   *
+   * @return array
+   *   An array of configuration object names that are editable if called in
+   *   conjunction with the trait's config() method.
+   */
+  protected function getEditableConfigNames() {
+    return ['hello_world.custom_salutation'];
+  }
+
+  /**
+   * Returns a unique string identifying the form.
+   *
+   * The returned ID should be a unique string that can be a valid PHP function
+   * name, since it's used in hook implementation names such as
+   * hook_form_FORM_ID_alter().
+   *
+   * @return string
+   *   The unique string identifying the form.
+   */
+  public function getFormId() {
+    return 'salutation_configuration_form';
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('hello_world.custom_salutation');
+    $form['salutation'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Salutation'),
+      '#description' => $this->t('Please provide the salutation you want to
+use.'),
+      '#default_value' => $config->get('salutation'),
+    ];
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->config('hello_world.custom_salutation')
+    ->set('salutation', $form_state->getValue('salutation'))
+    ->save();
+    parent::submitForm($form, $form_state);
+  }
+
+}
